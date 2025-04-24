a66fbdb36e1d9d3ed7da3d52ecd89b6de395c5ec
brunocosta010 <brunomatheusbmc157@gmail.com>
2025-04-24 20:40:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Nunca é tarde para recomeçar. Continue estudando e confie em você!";
+}
